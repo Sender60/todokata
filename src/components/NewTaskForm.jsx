475f0192216f2ee477a2640d2ac421cc273f380b
Sender60 <butkov60@gmail.com>
2025-01-19 04:09:15 +0300

    валидация формы

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -5,34 +5,14 @@ const NewTaskForm = ({ handleAddTask }) => {
   const [value, setValue] = useState('');
   const [minutes, setMinutes] = useState('');
   const [seconds, setSeconds] = useState('');
-  const [errorSeconds, setErrorSeconds] = useState('');
-  const [errorValue, setErrorValue] = useState('');
 
   const onKeyDown = (e) => {
     if (e.key === 'Enter' || e.type === 'submit') {
-      if (value === '' || minutes === '' || seconds === '') {
-        setErrorValue('Заполните все поля');
-        return;
-      }
-      if (minutes < 0 || seconds < 0) {
-        setErrorValue('Время не может быть отрицательным');
-        return;
-      }
       handleAddTask(value, minutes * 60 + Number(seconds));
       setValue('');
       setMinutes('');
       setSeconds('');
-      setErrorValue('');
     }
-  };
-
-  const handleSeconds = (e) => {
-    if (e.target.value > 59) {
-      setErrorSeconds('Не более 59 секунд');
-    } else {
-      setErrorSeconds('');
-    }
-    setSeconds(e.target.value);
   };
 
   return (
@@ -50,6 +30,7 @@ const NewTaskForm = ({ handleAddTask }) => {
           placeholder="What needs to be done?"
           onChange={(e) => setValue(e.target.value)}
           value={value}
+          required
         />
         <input
           type="number"
@@ -57,19 +38,23 @@ const NewTaskForm = ({ handleAddTask }) => {
           placeholder="Min"
           value={minutes}
           onChange={(e) => setMinutes(e.target.value)}
+          required
+          min={0}
         />
         <input
           type="number"
           className="new-todo-form__timer"
           placeholder="Sec"
           maxLength={2}
-          onChange={(e) => handleSeconds(e)}
+          onChange={(e) => setSeconds(e.target.value)}
           value={seconds}
+          required
+          min={0}
+          max={59}
+          title="Не более 59 секунд"
         />
         <button type="submit" style={{ display: 'none' }} aria-hidden />
       </form>
-      {errorSeconds && <p style={{ color: 'red' }}>{errorSeconds}</p>}
-      {errorValue && <p style={{ color: 'red' }}>{errorValue}</p>}
     </header>
   );
 };
